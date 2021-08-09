917b77fd4fdd4c8ce1d2a625c20a523684cd7da6
iasvobodin <iasvobodin@gmail.comm>
2021-08-09 07:10:31 +0500

    hj

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,24 +10,34 @@
 
 		rot = anime({
 			targets: '.title__background',
-			scale: [6, 1],
-			// translateX: '50%',
-			// rotate: 180,
+			scale: [3, 1],
+			easing: 'linear',
+			autoplay: false,
+			complete: (anim) => {
+				console.log(anim);
+			}
+		});
+		const words = anime({
+			targets: '.letters',
+			opacity: { value: 0, duration: 100 },
+			translateY: { value: '-100vh' }, //, delay: 800, duration: 200 },
+			delay: anime.stagger(40, { start: 400 }),
 			easing: 'linear',
 			autoplay: false
 		});
 		// console.log(rot);
 		scroll = new locomotiveModule.default({
 			el: scrollConatiner,
-			smooth: true
-			// smoothMobile: false
+			smooth: true,
+			smartphone: { smooth: true }
 		});
 		scroll.on('scroll', (func) => {
 			// Get all current elements : func.currentElements
 			if (typeof func.currentElements['hey'] === 'object') {
 				let progress = func.currentElements['hey'].progress;
-				console.log(progress);
+				// console.log(progress);
 				rot.seek(progress * 1000);
+				words.seek(progress * 3000);
 				// ouput log example: 0.34
 				// gsap example : myGsapAnimation.progress(progress);
 			}
@@ -36,13 +46,7 @@
 </script>
 
 <main bind:this={scrollConatiner}>
-	<section class="decription">
-		<div class="title">
-			<h1 class="title__hedline">
-				<span>Фотопроект</span>
-				<span>"Ничего лишнего"</span>
-			</h1>
-		</div>
+	<section data-scroll-section class="decription">
 		<div class="holder__bg">
 			<div
 				data-scroll-speed="6"
@@ -51,56 +55,98 @@
 				data-scroll-target="#hold"
 				class="imghold"
 			>
-				<img class="title__background" alt="SvobodinaPhoto" src="/img/1.jpg" />
+				<div class="title">
+					<h1 class="title__hedline">
+						{#each 'Фотопроект' as item}
+							<span class="letters">{item}</span>
+						{/each}
+						<br />{#each '"Ничего' as item}
+							<span class="letters">{item}</span>
+						{/each}
+						{#each 'лишнего"' as item}
+							<span class="letters">{item}</span>
+						{/each}
+					</h1>
+				</div>
+				<div class="title__background">
+					<img alt="SvobodinaPhoto" src="/img/1.jpg" />
+				</div>
+				<!-- <div class="gap" /> -->
+			</div>
+			<div class="description__title">
+				<h2>Фотографии на белом фоне,</h2>
 			</div>
 			<div id="hold" class="stick__triger" />
-			<div data-scroll data-scroll-id="hey" class="anim__triger" />
+			<div data-scroll data-scroll-id="hey" class="anim__triger__step1" />
+			<div data-scroll data-scroll-id="step2" class="anim__triger__step2" />
 
-			<div class="gap" />
 			<!-- <div data-scroll data-scroll-id="hey" class="trigger" /> -->
 		</div>
 	</section>
 </main>
 
 <style>
+	.letters {
+		display: inline-block;
+		white-space: break-spaces;
+	}
 	.imghold {
 		display: grid;
 		height: auto;
 	}
-	.anim__triger {
+	.stick__triger {
+		position: absolute;
+		height: 400vh;
+		width: 1px;
+		right: 0px;
+		top: 0;
+		border: 3px solid red;
+	}
+	.anim__triger__step1 {
 		position: absolute;
 		height: 100vh;
-		width: 5px;
-		right: 150px;
+		width: 1px;
+		right: 6px;
 		top: 100vh;
-		border: 1px solid green;
+		border: 3px solid green;
+	}
+	.anim__triger__step2 {
+		position: absolute;
+		height: 100vh;
+		width: 1px;
+		right: 9px;
+		top: 200vh;
+		border: 3px solid blue;
+	}
+	.description__title {
+		position: absolute;
+		top: 250vh;
+		left: 10vh;
+		font-size: min(36px, 8vw);
 	}
 	.gap {
 		height: 100vh;
 	}
 	.title__background {
-		z-index: -1;
-		place-self: end;
-		/* position: absolute; */
 		transform-origin: 100% 0%;
-		right: 0px;
+		transform: scale(6);
+		z-index: -1;
+		justify-self: end;
+		align-self: center;
+		height: 100vh;
+		width: 80vw;
 	}
 	.holder__bg {
 		/* margin-top: -100vh; */
 		position: relative;
 		height: 400vh;
 	}
-	.stick__triger {
-		position: absolute;
-		height: 300vh;
-		width: 5px;
-		right: 50px;
-		top: 0;
-		border: 1px solid red;
-	}
+
 	.title {
+		position: absolute;
 		/* transform: translateY(-100%); */
 		height: 100vh;
+		width: 100%;
 		display: grid;
 	}
 	.title__hedline {
@@ -110,17 +156,20 @@
 		font-size: max(36px, 10.8vw);
 	}
 	.title__hedline > span {
-		display: block;
-		width: 100%;
+		/* display: block; */
+		/* width: 100%; */
 	}
-	.title__hedline > span:first-child {
+	/* .title__hedline > span:first-child {
 		text-align: left;
 	}
 	.title__hedline > span:last-child {
 		text-align: right;
-	}
+	} */
 	img {
-		height: 100vh;
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+		object-position: 100% 0%;
 	}
 	.decription {
 		position: relative;
---
+++ src/routes/test.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+	let scrollConatiner;
+	onMount(async () => {
+		const locomotiveModule = await import('locomotive-scroll');
+
+		scroll = new locomotiveModule.default({
+			el: scrollConatiner,
+			smooth: true,
+			smartphone: { smooth: true }
+		});
+		scroll.on('scroll', (func) => {});
+	});
+</script>
+
+<main bind:this={scrollConatiner}>
+	<div class="test">
+		<div id="hold" data-scroll-sticky data-scroll-target="#hold" class="holder">
+			<img src="/img/1.jpg" alt="sv" />
+		</div>
+	</div>
+</main>
+
+<style>
+	.test {
+		margin-top: 50vh;
+		height: 400vh;
+	}
+	.holder {
+		height: 50vh;
+		width: calc(50vh * 0.66);
+	}
+	img {
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+		object-position: center;
+	}
+</style>
